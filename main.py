d2d2a318dd047700899c8abca897523c716a563d
lukas-sparnauskas <lukas.11sp@gmail.com>
2022-10-16 19:59:10 +0200

    feat: pca compression and uncompression

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import seaborn
 import argparse
 import subprocess
@@ -66,9 +67,17 @@ for row in data_file_S:
 data_S = np.array(data_S)
 
 
-################################
-### CALCULATING CORRELATIONS ###
-################################
+##################
+### UNIVARIATE ###
+##################
+
+####################
+### MULTIVARIATE ###
+####################
+
+    ################################
+    ### CALCULATING CORRELATIONS ###
+    ################################
 
 def find_correlations(N_features, dataset):
     correlations = []
@@ -97,13 +106,113 @@ def find_correlations(N_features, dataset):
 #         f'{features_S[corr[0]]} and {features_S[corr[1]]} correlate at a rate of', '%.3f' % corr[2])
 
 
-corr_R = pd.DataFrame(data_R, columns=features_R).corr()
-corr_S = pd.DataFrame(data_S, columns=features_S).corr()
+# corr_R = pd.DataFrame(data_R, columns=features_R).corr()
+# corr_S = pd.DataFrame(data_S, columns=features_S).corr()
 
-plt.figure(figsize=(20, 20))
-seaborn.heatmap(corr_R, cmap='YlGnBu')  # , mask=(np.abs(corr_R) >= 0.5))
-plt.show()
+# plt.figure(figsize=(20, 20))
+# seaborn.heatmap(corr_R, cmap='YlGnBu')  # , mask=(np.abs(corr_R) >= 0.5))
+# plt.show()
 
-plt.figure(figsize=(20, 20))
-seaborn.heatmap(corr_S, cmap='YlGnBu')  # , mask=(np.abs(corr_S) >= 0.5))
-plt.show()
+# plt.figure(figsize=(20, 20))
+# seaborn.heatmap(corr_S, cmap='YlGnBu')  # , mask=(np.abs(corr_S) >= 0.5))
+# plt.show()
+
+
+###########
+### PCA ###
+###########
+
+def center_data(A):
+    # INPUT:
+    # A    [NxM] numpy data matrix (N samples, M features)
+    #
+    # OUTPUT:
+    # X    [NxM] numpy centered data matrix (N samples, M features)
+    m = np.mean(A, axis=0)
+    X = A - m
+    return X
+
+def compute_covariance_matrix(A):
+    # INPUT:
+    # A    [NxM] centered numpy data matrix (N samples, M features)
+    #
+    # OUTPUT:
+    # C    [MxM] numpy covariance matrix (M features, M features)
+    #
+    # Do not apply centering here. We assume that A is centered before this function is called.
+    C = np.cov(np.transpose(A))
+    return C
+
+def compute_eigenvalue_eigenvectors(A):
+    # INPUT:
+    # A    [DxD] numpy matrix
+    #
+    # OUTPUT:
+    # eigval    [D] numpy vector of eigenvalues
+    # eigvec    [DxD] numpy array of eigenvectors
+    eigval, eigvec = np.linalg.eig(A)
+    # Numerical roundoff can lead to (tiny) imaginary parts. We correct that here.
+    eigval = eigval.real
+    eigvec = eigvec.real
+    return eigval, eigvec
+
+def sort_eigenvalue_eigenvectors(eigval, eigvec):
+    # INPUT:
+    # eigval    [D] numpy vector of eigenvalues
+    # eigvec    [DxD] numpy array of eigenvectors
+    #
+    # OUTPUT:
+    # sorted_eigval    [D] numpy vector of eigenvalues
+    # sorted_eigvec    [DxD] numpy array of eigenvectors
+    indices = np.argsort(eigval)
+    indices = indices[::-1]
+    sorted_eigval = eigval[indices]
+    sorted_eigvec = eigvec[:,indices]
+    return sorted_eigval, sorted_eigvec
+
+def encode_decode_pca(A,m):
+    # INPUT:
+    # A    [NxM] numpy data matrix (N samples, M features)
+    # m    integer number denoting the number of learned features (m <= M)
+    #
+    # OUTPUT:
+    # Ahat [NxM] numpy PCA reconstructed data matrix (N samples, M features)
+    me = np.mean(A, axis=0)
+    A = center_data(A)
+    C = compute_covariance_matrix(A)
+    eigval, eigvec = compute_eigenvalue_eigenvectors(C)
+    eigval, eigvec = sort_eigenvalue_eigenvectors(eigval, eigvec)
+    if m > 0:
+        eigvec = eigvec[:,:m]
+    for i in range(np.shape(eigvec)[1]):
+        eigvec[:,i] / np.linalg.norm(eigvec[:,i]) * np.sqrt(eigval[i])
+    P = np.dot(np.transpose(eigvec), np.transpose(A))
+    Ahat = np.dot(P[:,:m].T, eigvec.T)
+    Ahat += me  
+    return Ahat
+
+PCA_R = encode_decode_pca(data_R, math.floor(N_R / 4))
+
+corr_PCA = pd.DataFrame(PCA_R).corr()
+corr_OR = pd.DataFrame(data_R).corr()
+
+# plt.figure(figsize=(20, 20))
+# seaborn.heatmap(corr_PCA, cmap='YlGnBu')  # , mask=(np.abs(corr_R) >= 0.5))
+# plt.title('PCA (with N/2 of features) compressed and decompressed corrs')
+# plt.show()
+
+# plt.figure(figsize=(20, 20))
+# seaborn.heatmap(corr_OR, cmap='YlGnBu')  # , mask=(np.abs(corr_S) >= 0.5))
+# plt.title('Original corrs')
+# plt.show()
+
+diff_corrs = corr_PCA.sub(corr_OR).abs()
+print('Difference between corrs: \n', diff_corrs)
+
+##########
+### ML ###
+##########
+
+###############
+### COPULAS ###
+###############
