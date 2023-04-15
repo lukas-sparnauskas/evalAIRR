88b60b40d7e77c135d06b037b1946414973b0b9c
lukas-sparnauskas <lukas.11sp@gmail.com>
2023-04-15 22:55:28 +0200

    fix: fixed threshold test

--- tests/run_threshold_tests.py
+++ tests/run_threshold_tests.py
@@ -10,23 +10,9 @@ import matplotlib.pyplot as plt
 
 from yaml_files import threshold_test_immuneml_spec, threshold_test_evalairr_spec
 
-max_reps = 50
-max_seqs = 100
+max_reps = 200
+max_seqs = 100000
 n_runs = 10
-thresholds = {
-    'ks': 0.4,
-    'ks_pval': 0.8,
-    'dist': 3,
-    'dist_obs': 10,
-    'avg': 0.1,
-    'avg_obs': 0.2,
-    'median': 0.5,
-    'median_obs': 0.3,
-    'var': 0.3,
-    'var_obs': 0.3,
-    'std': 1,
-    'std_obs': 0.2
-}
 
 ks_results = []
 
@@ -45,14 +31,14 @@ for n in range(0, n_runs):
     ### CREATE METADATA FILES
     
     print(f'[LOG] CREATING METADATA FILES')
-    data_1_list = os.listdir('/home/mint/masters/data/real_data/repertoires/')
-    data_2_list = os.listdir('/home/mint/masters/data/real_data/repertoires/')
+    data_1_list = sorted(os.listdir('/home/mint/masters/data/real_data/repertoires/'))[:563]
+    data_2_list = sorted(os.listdir('/home/mint/masters/data/real_data/repertoires/'))[:563]
     rnd.shuffle(data_1_list)
     rnd.shuffle(data_2_list)
     n_reps = min(max_reps, len(data_1_list), len(data_2_list))
     
     data_1_list = np.array(data_1_list, dtype=str)[:n_reps]
-    with open(f'/home/mint/masters/data/real_data/metadata_{output_timestamp}.csv', 'w') as file:
+    with open(f'/home/mint/masters/data/th_data_1/metadata_{output_timestamp}.csv', 'w') as file:
         file.write('filename,subject_id\n')
         for r in data_1_list:
             subject_id = r.replace('.tsv', '')
@@ -66,7 +52,7 @@ for n in range(0, n_runs):
             data.close()
             
     data_2_list = np.array(data_2_list, dtype=str)[:n_reps]
-    with open(f'/home/mint/masters/data/real_data/metadata_{output_timestamp}.csv', 'w') as file:
+    with open(f'/home/mint/masters/data/th_data_2/metadata_{output_timestamp}.csv', 'w') as file:
         file.write('filename,subject_id\n')
         for r in data_2_list:
             subject_id = r.replace('.tsv', '')
@@ -88,14 +74,14 @@ for n in range(0, n_runs):
     subprocess.run(f'sudo mkdir /home/mint/masters/data/th_data_2/temp', shell=True)
     n_seq = min(max_seqs, min_n_seq)
     for r in data_1_list:
-        source = open(f'/home/mint/masters/data/th_data_1/repertoires/{r}', 'r')
+        source = open(f'/home/mint/masters/data/real_data/repertoires/{r}', 'r')
         rows = [next(source) for _ in range(n_seq + 1)]
         with open(f'/home/mint/masters/data/th_data_1/temp/{r}', 'w') as target:
             target.writelines(rows)
         source.close()
         
     for r in data_2_list:
-        source = open(f'/home/mint/masters/data/th_data_2/repertoires/{r}', 'r')
+        source = open(f'/home/mint/masters/data/real_data/repertoires/{r}', 'r')
         rows = [next(source) for _ in range(n_seq + 1)]
         with open(f'/home/mint/masters/data/th_data_2/temp/{r}', 'w') as target:
             target.writelines(rows)
@@ -145,42 +131,11 @@ for t in timestamps:
 ### CALCULATIONS
 final_stat = dict()
 final_stat_obs = dict()
-t_results = {
-    'ks': [],
-    'ks_pval': [],
-    'dist': [],
-    'dist_obs': [],
-    'avg': [],
-    'avg_obs': [],
-    'median': [],
-    'median_obs': [],
-    'var': [],
-    'var_obs': [],
-    'std': [],
-    'std_obs': []
-}
 for t in timestamps:
     final_stat[t] = np.absolute(final_stat_R[t] - final_stat_S[t])
     final_stat_obs[t] = np.absolute(final_stat_obs_R[t] - final_stat_obs_S[t])
-    
-    t_results['ks'].append(float(np.count_nonzero(np.where(final_ks[t][0] <= thresholds['ks'], final_ks[t][0], 0))) / len(final_ks[t][0]))
-    t_results['ks_pval'].append(float(np.count_nonzero(np.where(final_ks[t][1] >= thresholds['ks_pval'], final_ks[t][1], 0))) / len(final_ks[t][1]))
-    t_results['dist'].append(float(np.count_nonzero(np.where(final_dist[t][0] <= thresholds['dist'], final_dist[t][0], 0))) / len(final_dist[t][0]))
-    t_results['dist_obs'].append(float(np.count_nonzero(np.where(final_dist_obs[t][0] <= thresholds['dist_obs'], final_dist_obs[t][0], 0))) / len(final_dist_obs[t][0]))
-    t_results['avg'].append(float(np.count_nonzero(np.where(final_stat[t][0] <= thresholds['avg'], final_stat[t][0], 0))) / len(final_stat[t][0]))
-    t_results['avg_obs'].append(float(np.count_nonzero(np.where(final_stat_obs[t][0] <= thresholds['avg_obs'], final_stat_obs[t][0], 0))) / len(final_stat_obs[t][0]))
-    t_results['median'].append(float(np.count_nonzero(np.where(final_stat[t][1] <= thresholds['median'], final_stat[t][1], 0))) / len(final_stat[t][1]))
-    t_results['median_obs'].append(float(np.count_nonzero(np.where(final_stat_obs[t][1] <= thresholds['median_obs'], final_stat_obs[t][1], 0))) / len(final_stat_obs[t][1]))
-    t_results['std'].append(float(np.count_nonzero(np.where(final_stat[t][2] <= thresholds['std'], final_stat[t][2], 0))) / len(final_stat[t][2]))
-    t_results['std_obs'].append(float(np.count_nonzero(np.where(final_stat_obs[t][2] <= thresholds['std_obs'], final_stat_obs[t][2], 0))) / len(final_stat_obs[t][2]))
-    t_results['var'].append(float(np.count_nonzero(np.where(final_stat[t][3] <= thresholds['var'], final_stat[t][3], 0))) / len(final_stat[t][3]))
-    t_results['var_obs'].append(float(np.count_nonzero(np.where(final_stat_obs[t][3] <= thresholds['var_obs'], final_stat_obs[t][3], 0))) / len(final_stat_obs[t][3]))
 
 ### FINAL RESULT FIGURE EXPORT
-for key in ['ks_pval', 'dist', 'dist_obs', 'avg', 'avg_obs', 'median', 'median_obs', 'var', 'var_obs', 'std', 'std_obs']:
-    ab_be = 'above' if key == 'ks_pval' else 'below'
-    print(f'[RESULT] Indicator:{key} - % {ab_be} threshold: {np.average(t_results[key]) * 100}')
-
 colours = sns.color_palette(cc.glasbey, n_runs).as_hex()
 def draw_kdeplot(data, title, xlabel, output, stat=None):
     f, ax = plt.subplots(1, 1)
@@ -198,67 +153,67 @@ def draw_kdeplot(data, title, xlabel, output, stat=None):
 # KS Statistic
 title = 'Distribution of KS statistic for each iteration'
 xlabel = 'Kolmogorov-Smirnov statistic'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_ks.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_ks.png'
 draw_kdeplot(final_ks, title, xlabel, output, 0)
 
 # KS P-values
 title = 'Distribution of KS P-values for each iteration'
 xlabel = 'Kolmogorov-Smirnov P-values'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_ks_pval.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_ks_pval.png'
 draw_kdeplot(final_ks, title, xlabel, output, 1)
 
 # Euclidean distance
 title = 'Distribution of Euclidean distance between \nfeatures for each iteration'
 xlabel = 'Euclidean distance between features'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_dist.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_dist.png'
 draw_kdeplot(final_dist, title, xlabel, output)
 
 # Observation Euclidean distance
 title = 'Distribution of Euclidean distance between\nobservations for each iteration'
 xlabel = 'Euclidean distance between observations'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_dist_obs.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_dist_obs.png'
 draw_kdeplot(final_dist_obs, title, xlabel, output)
 
 # Statistics
 title = 'Distribution of the difference between the\naverage feature values for each iteration'
 xlabel = 'Difference between the average feature values'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_average.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_average.png'
 draw_kdeplot(final_stat, title, xlabel, output, 0)
 
 title = 'Distribution of the difference between the\nmedian feature values for each iteration'
 xlabel = 'Difference between the median feature values'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_median.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_median.png'
 draw_kdeplot(final_stat, title, xlabel, output, 1)
 
 title = 'Distribution of the difference between the\nfeature standard deviation for each iteration'
 xlabel = 'Difference between the feature standard deviation'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_std.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_std.png'
 draw_kdeplot(final_stat, title, xlabel, output, 2)
 
 title = 'Distribution of the difference between the\nfeature variance for each iteration'
 xlabel = 'Difference between the feature variance'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_var.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_var.png'
 draw_kdeplot(final_stat, title, xlabel, output, 3)
 
 # Observation statistics
 title = 'Distribution of the difference between the\naverage observation values for each iteration'
 xlabel = 'Difference between the average observation values'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_obs_average.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_obs_average.png'
 draw_kdeplot(final_stat_obs, title, xlabel, output, 0)
 
 title = 'Distribution of the difference between the\nmedian observation values for each iteration'
 xlabel = 'Difference between median observation values'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_obs_median.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_obs_median.png'
 draw_kdeplot(final_stat_obs, title, xlabel, output, 1)
 
 title = 'Distribution of the difference between the\nobservation standard deviation for each iteration'
 xlabel = 'Difference between the observation standard deviation'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_obs_std.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_obs_std.png'
 draw_kdeplot(final_stat_obs, title, xlabel, output, 2)
 
 title = 'Distribution of the difference between the\nobservation variance for each iteration'
 xlabel = 'Difference between the observation variance'
-output = f'/home/mint/masters/data/evalairrdata/run_{run_timestamp}/results_stat_obs_var.png'
+output = f'/home/mint/masters/data/evalairrdata/th_run_{run_timestamp}/results_stat_obs_var.png'
 draw_kdeplot(final_stat_obs, title, xlabel, output, 3)
 
 print(f'[LOG] EXECUTION TIME {(time.time() - start_time) / 60} m')
